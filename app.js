b1cd27ed4ac4b6a542b60b86c05b2aff516e5b65
juanjo <alu0100721805@ull.edu.es>
2019-03-23 00:21:49 +0000

    Se añada deteccion de errores para entornos nos productivos y midleware  para status 500

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-var express = require('express'),
-    config = require('./config'),
+const express = require('express'),
+    errorHandler = require('errorhandler');
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
+    cors = require('cors'),
     tagrouter = require('./router/TagRouter'),
     userrouter = require('./router/UserRouter'),
-    app = express();
-
+    config = require('./config');
+mongoose.promise = global.Promise;
 app.set('views', './views')
 app.set('view engine', 'pug');
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ 'strict': true }));
 app.use('/static', express.static(__dirname + '/public'));
 
+//is Production ?
+const isProduction = process.env.NODE_ENV === 'production';
+
+if(!isProduction) {
+    app.use(errorHandler());
+  }
+  
 const connectionString = "mongodb://" + config.db.host + ":" + config.db.port + "/" + config.db.name;
 const  options = {
         useMongoClient : true,
@@ -20,16 +30,11 @@ const  options = {
         poolSize: 10, 
         bufferMaxEntries: 0
 };
-var gracefulExit = function() {
-    mongoose.connection.close(function() {
-        console.log('Se cierra la conexión de la base de datos dado que se ha terminado el proceso');
-        process.exit(0);
-    });
-}
 
-// Si se mata el proceso de node entonces se desconecta de la base de datos
+// EVENTS 
+
+// If we kill the process then down bbdd conections
 process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
-
 //Eventos de conexión a la base de datos
 mongoose.connection.on('connected', function() {
     console.log('Conectado a ' + connectionString);
@@ -47,8 +52,39 @@ mongoose.connection.on('disconnected', function() {
     console.log('¡Se ha desconectado de la base de datos!');
 }, gracefulExit);
 
+
+
+ // CONECTED OR NOT 
  mongoose.connect(connectionString, options);
 
+//Error handlers & middlewares
+if(!isProduction) {
+    app.use((err, req, res) => {
+      res.status(err.status || 500);
+  
+      res.json({
+        errors: {
+          message: err.message,
+          error: err,
+        },
+      });
+    });
+  }
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.json({
+      errors: {
+        message: err.message,
+        error: {},
+      },
+    });
+  });
 
-
+ // Utils
+ var gracefulExit = function() {
+    mongoose.connection.close(function() {
+        console.log('Se cierra la conexión de la base de datos dado que se ha terminado el proceso');
+        process.exit(0);
+    });
+}
 module.exports = app;
