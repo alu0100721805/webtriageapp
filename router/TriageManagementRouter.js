16d772fd3f99391a5f1c58da4efc02497f2c355b
JJ <alu0100721805@ull.edu.es>
2019-09-20 08:12:03 +0100

    Ultimos cambios

--- router/TriageManagementRouter.js
+++ router/TriageManagementRouter.js
@@ -6,6 +6,7 @@ var tag_controller = require('../controllers/TriageManagementController');
 router.get('/tag/:id', tag_controller.tag_findById_get);
 router.get('/tag/:colour', tag_controller.tag_findByColour_get);
 router.post('/tag/create', tag_controller.tag_create_post);
+router.post('/tiage/create', )
 router.all('/', tag_controller.index_map_management);
 
 module.exports = router;
